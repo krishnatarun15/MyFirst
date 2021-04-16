ce3bbd898e21ff74ef17c692994a1ebffbbb23dd
krishnatarun15 <73231989+krishnatarun15@users.noreply.github.com>
2021-04-16 14:10:48 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,280 @@
-#include <calculator_operations.h>
-
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+FILE *p,*fp;
+int selection[15],q;
+struct cafe
 {
-    printf("\n****Calculator****\n");
-    while(1)
-    {
-        calculator_menu();
-    }
+	int slno;
+	char name[15];
+	int price;
+}item;
+struct payments
+{
+	int slno1;
+	char name1[15];
+	int price1;
+}bill; 
+
+void admin();
+void user();
+void addition();
+void menu();
+void search();
+void billing();
+void payments();
+void back();
+void deletingitems();
+
+
+void admin(){
+	int i;
+A:
+	printf("\t\t 1.ADD NEW ITEM TO MENU\n\n");
+	printf("\t\t 2.REMOVE ITEM FROM MENU\n\n");
+	printf("\t\t 3.VIEW PAYMENTS\n\n");
+	printf("\t\t 4.BACK TO LOGIN\n\n");
+	printf("\t\t 5.VIEW MENU\n\n");
+	
+	printf("\t\t 6.EXIT\n");
+	scanf("%d",&i);
+	switch(i)
+	{
+		case 1:
+			addition();
+			goto A;
+			break;
+	    case 2:
+	    	
+	    	deletingitems();
+			goto A;
+	    	break;
+	    case 3:
+		    payments();
+		    goto A;
+			break;
+		case 4:
+			back();
+			goto A;
+			break;
+	    case 5:
+		    menu();
+			goto A;
+			break;		
+		case 6:
+			exit(0);
+		default:
+		   printf("INVALID INPUT\n");	     		
+	}
+	printf("CHOOSE OPTION NO.:\n");
 }
-
-void calculator_menu(void)
+void menu()
 {
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
+	int m;
+	printf("\tItem Sl.no\t\tItem Name\t\tItem Price\n");
+	fp=fopen("menu.txt","r");
+	if(fp==NULL)
+	{
+		printf("cannot open file\n");
+	}
+	else
+	{
+	
+    while(	fread(&item,sizeof(item),1,fp))
+	{
+		printf("\t%d\t\t\t%s\t\t\t%d\n",item.slno,item.name,item.price);
+	}
+}
+fclose(fp);
+}
+void user()
+{
+	int k=0,count;
+	char ch,z;
+	printf("\t\tREFER MENU\n\n");
+	menu();
+	printf("DO YOU WANT TO SEARCH FOR ITEM ? (Y/N) \n");
+	ch=getch();
+	if( ch == 'Y' || ch == 'y')
+	{
+		search();
+	}
+	printf("You have choosed N\n");
+	printf("\nSELECT ITEMS FROM MENU\n");
+
+	billing();
+}
+void billing()
+{
+	int y=0,i=0,total=0,selection[15];
+	char ch='Y';
+	
+	fflush(stdin);
+	while(ch!='N')
+	{
+		printf("ENTER SERIAL NO. OF ITEM:\n");
+		scanf("%d",&selection[i]);
+		y++;
+		printf("WANT TO SELECT ANY OTHER?(Y)  if not(N)\n");
+		fflush(stdin);
+		scanf("%c",&ch);
+		i++;
+	}
+		fflush(stdin);
+
    
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
-
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
+	p=fopen("bill.txt","a");
+	fp=fopen("menu.txt","r");
+	printf("\tItem Sl.no\t\tItem Name\t\tItem Price\n");
+	
+	i=0;
+	
+	while(fread(&item,sizeof(item),1,fp))
+	{
+		if(item.slno==selection[i]){
+		printf("\t%d\t\t\t%s\t\t\t%d\n\n",item.slno,item.name,item.price);
+		
+		total=total+ item.price;
+		fwrite(&bill,sizeof(bill),1,p);
+		i++;
+		
+	}
+	
     }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
+	 
+    fflush(stdin);
+	printf("\n\n\n\t TOTAL=%d\n",total);
+	fclose(fp);
+	fclose(p);
+	printf("\n\n");
+	admin();
 }
-
-int valid_operation(int operation)
+void addition()
 {
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+	int b,i,j=0;
+	p=fopen("menu.txt","a");
+	printf("HOW MANY ITEMS YOU WANT TO ADD?\n");
+	scanf("%d",&b);
+	q=q+b;
+		for(i=0;i<b;i++){
+		printf("Serial number:");
+		scanf("%d",&item.slno);
+        printf("Item name:");
+        scanf("%s",&item.name);
+        printf("Item Price:");
+        scanf("%d",&item.price);
+        fwrite(&item,sizeof(item),1,p);
+        fflush(stdin);
 }
+
+	fclose(p);
+	printf("ITEM(S) ADDED SUCCESSFULLY\n");
+}
+
+
+
+void deletingitems()
+	{
+		int f;
+		printf("\nPlease Enter serial number to delete :");
+		scanf("%d",&f);
+		FILE *fp,*fp1;
+	    fp=fopen("menu.txt","r");
+		fp1=fopen("delitems.txt","w");
+		while(fread(&item,sizeof(item),1,fp))
+		{
+		if(item.slno==f)
+		{
+			printf("DELETED");
+		}
+		else
+		{
+			fwrite(&item,sizeof(item),1,fp1);
+		}
+		}
+		fclose(fp);
+    	fclose(fp1);	
+    	remove("menu.txt");
+    	rename("delitems.txt","menu.txt");
+    	printf("\n\n\n");
+    }
+void search()
+{
+	fflush(stdin);
+	char element[10];
+	int i=0;
+	printf("You have choosen Y.\n");
+	printf("Enter name of item that you want to search\n");
+	gets(element);
+	p=fopen("menu.txt","r");
+while(fread(&item,sizeof(item),1,p)){
+
+	if((stricmp(element,item.name))==0){
+		printf("ITEM SL_NO.\t ITEM NAME \t ITEM PRICE\n");
+		printf("\t%d\t\t%s\t\t%d",item.slno,item.name,item.price);
+	}
+	
+}
+}
+void payments()
+{
+	printf("\t\t\t CAFE MANAGEMENT SYSTEM\t\t\t\n\n");
+	printf("\tItem Sl.no\t\tItem Name\t\tItem Price\n");
+	printf("\t==========\t\t=========\t\t==========\n");
+	p=fopen("bill.txt","r");
+	while(fread(&bill,sizeof(bill),1,p)){
+		printf("\t%d\t\t\t%s\t\t\t%d\n\n",item.slno,item.name,item.price);
+	}
+	fclose(p); 
+	admin();
+}
+
+void introduction()
+{
+	printf("\n\n\n\t\t\t-------------------------------WELCOME------------------------------\n");
+	printf("\n\n\t\t\t\t\t\t\tCAFE  ");
+	printf("\n\t\t\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~");
+	
+	printf("\n\n\n\n\n");
+	printf("\n\t\t\t\t\t\t\t\t\t\t\tSubmitted By ");
+	printf("\n\t\t\t\t\t\t\t\t\t\t\tName :Management CAFE ");
+}
+
+
+
+int main()
+{
+ 
+ introduction();
+ getch();
+ back();
+ return 0;
+}
+ void back(){
+ 	int j;
+  system("cls");
+  
+	printf("\n\n\n\t\t\t-------------------------------WELCOME------------------------------\n");
+	printf("\n\n\t\t\t\t\t\tCAFE  ");
+	printf("\n\t\t\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~");
+	printf("\n\n\n");
+ printf("\t\t1.ADMIN\n");
+ printf("\t\t2.USER\n");
+ fflush(stdin);
+ scanf("%d",&j);
+ 
+ switch(j)
+ {
+ 	case 1:
+ 		admin();
+ 		break;
+ 	case 2:
+ 		user();
+ 		break;
+ 	default :
+ 		printf("INVALID INPUT\n");
+ 		break;
+ }
+}
+ 
